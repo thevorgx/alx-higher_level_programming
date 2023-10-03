b5b7b325e4f002299d0db1b12d1d0c5e8792de3c
thevorgx <kh.elallali@gmail.com>
2023-10-03 23:23:41 +0100

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-numbers = "00, 01, 02, 03, 04, 05, 06, 07, 08, 09"
-print("{}".format(numbers), end='')
-for i in range(10, 100):
-    print(", {}".format(i), end='')
+for i in range(100):
+    print(f"{i:02d}", end=', ' if i < 99 else '\n')
