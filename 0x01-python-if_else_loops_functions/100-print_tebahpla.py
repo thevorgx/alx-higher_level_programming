9905855d9963222243801946bd33ff4f9d28e3d5
thevorgx <kh.elallali@gmail.com>
2023-10-04 23:53:12 +0100

    update file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1):
-    print("{0}{1}".format(chr(i + 32 * (i % 2)), chr(i)), end="")
+for i in range(122, 96, -1):
+    print("{0}{1}".format(chr(i - 32 * (i % 2)), chr(i)), end="")
+
