abe76f3bf4801fa85af7d608f91f0dd8a4df637c
thevorgx <kh.elallali@gmail.com>
2023-10-05 14:28:28 +0100

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0 as vorg
-a = 1
-b = 2
-v = vorg.add(a, b)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    v = add(a, b)
 
 print("{} + {} = {}".format(a, b, v))
