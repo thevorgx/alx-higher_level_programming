59c31c03a5975c946dd2b34169693bbc54d09dbe
thevorgx <kh.elallali@gmail.com>
2023-10-05 02:36:35 +0100

    100% working

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if b < a:
+    if a < b:
         return (c)
-    elif b > a:
+    elif c > b:
         return (a + b)
     else:
-        return ((a*b*c) - b)
+        return (a*b - c)
+    
