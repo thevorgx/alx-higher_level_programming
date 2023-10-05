181772627a5fa5f0a5d1391ba940be91d8f872c2
thevorgx <kh.elallali@gmail.com>
2023-10-05 17:09:36 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
             len(sys.argv) == 1
             count += 1
             print("{}: {}".format(count, i))
-    else:
+    elif len(sys.argv) == 0:
         print("{} arguments.".format(len(sys.argv) - 1))
