a0ac1e8ced88b8aafa143d2fa962f46487bcd511
thevorgx <kh.elallali@gmail.com>
2023-10-05 17:06:07 +0100

    update correct output (space)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
         for i in sys.argv[1:]:
             len(sys.argv) == 1
             count += 1
-            print("{} : {}".format(count, i))
+            print("{}: {}".format(count, i))
     else:
         print("{} arguments.".format(len(sys.argv) - 1))
