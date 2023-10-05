b6bdde041cc7b9176d9836c893ca66e9746507a7
thevorgx <kh.elallali@gmail.com>
2023-10-05 17:33:56 +0100

    updqte

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,14 @@
 if __name__ == "__main__":
     import sys
     count = 0
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv[1:]:
             len(sys.argv) == 1
             count += 1
             print("{}: {}".format(count, i))
+    elif len(sys.argv) > 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments.".format(len(sys.argv) - 1))
