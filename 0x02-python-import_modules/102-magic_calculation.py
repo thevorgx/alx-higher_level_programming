15d0e943e135d150bf5911d5a779e9e4a11e8b18
thevorgx <kh.elallali@gmail.com>
2023-10-06 04:00:23 +0100

    pycode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from magic_calculation_102 import add, sub
 
-def magic_calculation(a, b):
 
+def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
