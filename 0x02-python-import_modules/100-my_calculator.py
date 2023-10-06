2097818d9226019fcd71ee63ddca25370771e9cc
thevorgx <kh.elallali@gmail.com>
2023-10-06 03:21:46 +0100

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        res = add(a, b)
+    elif operator == '-':
+        res = sub(a, b)
+    elif operator == '*':
+        res = mul(a, b)
+    elif operator == '/':
+        res = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+    print("{} {} {} = {}".format(a, operator, b, res))
