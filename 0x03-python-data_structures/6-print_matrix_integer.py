03914f9b4431501c2ace53d1f31616a249cfcf2f
thevorgx <kh.elallali@gmail.com>
2023-10-09 17:32:38 +0100

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for row in matrix:
+            for integer in row:
+                print("{}".format(integer), end=' ')
+            print()
