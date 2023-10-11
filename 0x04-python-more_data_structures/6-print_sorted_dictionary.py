6e5a98ca633c7be52cd0252e2ff386329ca7852b
thevorgx <kh.elallali@gmail.com>
2023-10-12 00:05:48 +0100

    update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    dict0 = (a_dictionary.copy())
-    if 'Number' in dict0:
-        number = dict0['Number']
-        print("Number: {}".format(number))
-    else:
-        print("{}".format())
-
-    if 'ids' in dict0:
-        ids = dict0['ids']
-        print("ids: {}".format(ids))
-    else:
-        print("{}".format())
-
-    if 'language' in dict0:
-        language = dict0['language']
-        print("language: {}".format(language))
-    else:
-        print("{}".format())
-
-    if 'track' in dict0:
-        track = dict0['track']
-        print("track: {}".format(track))
-    else:
-        print("{}".format())
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        value = a_dictionary.get(key)
+        print("{}: {}".format(key, value))
