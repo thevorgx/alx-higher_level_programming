fa5c4623ecc4f9ff19de92a06f248802ac8f6af4
thevorgx <kh.elallali@gmail.com>
2023-10-11 20:55:31 +0100

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    res = 0
     if len(my_list) != 0:
         new_list = set(my_list)
-        res = sum(new_list)
+        for number in new_list:
+            res += number
         return (res)
+    else:
+        return ([])
