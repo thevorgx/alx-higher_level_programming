6d3f878ae63887a8c744cc9b457321c1c4b7fdae
thevorgx <kh.elallali@gmail.com>
2023-10-11 19:44:19 +0100

    task1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) != 0:
+        new_list = my_list.copy()
+        for idx in range(len(new_list) - 1):
+            if new_list[idx] == search:
+                new_list[idx] = replace
+        return (new_list)
+    else:
+        return
