5385dc8941805098f508e67cfac2e4c80ad9af75
thevorgx <kh.elallali@gmail.com>
2023-10-11 19:06:23 +0100

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     res = []
-    if len(matrix[0]) != 0:
+    if len(matrix) != 0:
         res = [[idx ** 2 for idx in row] for row in matrix]
         return (res)
     else:
-        return (res)
+        return
