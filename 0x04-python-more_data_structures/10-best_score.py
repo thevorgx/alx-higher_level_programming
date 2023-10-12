94882a6e533b3e5ef1c99cf9b499213398551bf5
thevorgx <kh.elallali@gmail.com>
2023-10-12 02:06:31 +0100

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    maxvalue = 0
+    best_key = 0
+    if a_dictionary is not None:
+        for key, value in a_dictionary.items():
+            if value > maxvalue:
+                maxvalue = value
+                best_key = key
+        return (best_key)
+    else:
+        return (None)
