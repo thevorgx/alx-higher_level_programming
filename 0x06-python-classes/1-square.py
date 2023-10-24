645c458ea01f7c66f7d355d258ad973e8c766580
thevorgx <kh.elallali@gmail.com>
2023-10-24 20:38:48 +0100

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Define a class named Square to represent a square."""
+
+
+class Square:
+    """
+    Constructor method to initialize a Square object.
+
+    Parameters:
+        size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+        Initialize a new Square with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size__ = size
