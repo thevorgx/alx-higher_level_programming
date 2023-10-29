e7dc8a1c4888e91ead623bcd063ad70458a03852
thevorgx <kh.elallali@gmail.com>
2023-10-29 16:03:05 +0100

    test cases for task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def add_integer(a, b=98):
+    if not (type(a) in (int, float)):
+        raise TypeError("a must be an integer")
+    if not (type(b) in (int, float)):
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a + b)
