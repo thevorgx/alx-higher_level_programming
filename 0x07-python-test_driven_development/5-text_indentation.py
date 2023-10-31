6ffdc46a36bd4d1eaa6bd9a8adeb12fe14ee3d1c
thevorgx <kh.elallali@gmail.com>
2023-11-01 00:08:27 +0100

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these chars: '.' '?' ':'"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    special_chars = set(['.', '?', ':'])
+    result = []
+    paragraph = []
+
+    for char in text:
+        paragraph.append(char)
+
+        if char in special_chars:
+            result.append(''.join(paragraph))
+            paragraph = []
+
+    if paragraph:
+        result.append(''.join(paragraph))
+
+    for paragraph in result:
+        print(paragraph.strip())
+        print()
