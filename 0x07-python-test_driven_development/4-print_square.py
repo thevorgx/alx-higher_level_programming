8dc297b678cdd7835c8ec264708ebaee81348fc7
thevorgx <kh.elallali@gmail.com>
2023-11-02 00:58:47 +0100

    revse changes

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" print square """
+"""print square"""
 
 
 def print_square(size=10):
-    """ print square """
-    if type(size) is not int:
+    """print square"""
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(0, size):
-        for j in range(0, size):
+    for i in range(size):
+        for j in range(size):
             if j != (size - 1):
-                print("#", end="")
+                print('#', end='')
             else:
                 print("#")
