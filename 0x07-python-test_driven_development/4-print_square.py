d51da9323a54bd32f4bae381ec2d7888be238bed
thevorgx <kh.elallali@gmail.com>
2023-11-02 00:49:31 +0100

    reverse changes

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,6 +14,5 @@ def print_square(size=10):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            if j != (size - 1):
-                print('#', end='')
+            print('#', end='')
         print()
