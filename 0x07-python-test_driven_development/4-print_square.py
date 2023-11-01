b4c1c6db7c2f3763c59db415b30827c999267810
thevorgx <kh.elallali@gmail.com>
2023-11-02 00:48:24 +0100

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,5 +14,6 @@ def print_square(size=10):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            print('#', end='')
+            if j != (size - 1):
+                print('#', end='')
         print()
