d8edbef4537e51c286de41d3890f21278a7afd93
thevorgx <kh.elallali@gmail.com>
2023-11-07 00:58:01 +0100

    task 11 test

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""import Square classe"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square child class"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area implemented"""
+        return (self.__size ** 2)
