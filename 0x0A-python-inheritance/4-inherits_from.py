29dfe38cd3b05e517b8e3b8ef28d4be9735fbacf
thevorgx <kh.elallali@gmail.com>
2023-11-06 10:15:54 +0100

    update 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""inherits from"""
+
+
 def inherits_from(obj, a_class):
+    """inherits from"""
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return (True)
     else:
