8e88d8ee63f4d492e12d29158a026b6e38611078
thevorgx <kh.elallali@gmail.com>
2023-11-06 23:23:31 +0100

    update pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,17 +25,18 @@ class BaseGeometry:
 class Rectangle(BaseGeometry):
     """Rectangle child class"""
 
-
     def __init__(self, width, height):
         """Instantiation with width and height"""
         self.__width = width
         self.__height = height
         self.integer_validator("height", height)
         self.integer_validator("width", height)
+
     def area(self):
         """area implemented"""
         res = self.__width * self.__height
         return (res)
+
     def __str__(self):
         """str"""
         return (f"[Rectangle] {self.__width}/{self.__height}")
