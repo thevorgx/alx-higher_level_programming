355971f6be74e9a01fa97da609dc46ff98146148
thevorgx <kh.elallali@gmail.com>
2023-11-07 16:25:43 +0100

    update task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""from json to string"""
+
+
 import json
+
+
 def from_json_string(my_str):
+    """from json to string"""
     j_string = json.loads(my_str)
-    return(j_string)
+    return (j_string)
