5c675212c631b926aaac5d1c07c16e4638f05924
thevorgx <kh.elallali@gmail.com>
2023-11-07 05:40:24 +0100

    task 3

---
+++ 0x0B-python-input_output/3-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+to_json_string = __import__('3-to_json_string').to_json_string
+
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+import json
+def to_json_string(my_obj):
+    j_string = json.dumps(my_obj)
+    return(j_string)
