bd5be40dcbc867d06f123208a1b31f4bf45590a4
thevorgx <kh.elallali@gmail.com>
2023-11-07 16:31:05 +0100

    update task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""load from json file"""
+
+
 import json
+
+
 def load_from_json_file(filename):
+    """load from json file"""
     with open(filename, 'r', encoding='UTF-8') as f:
         j_string = json.load(f)
         return (j_string)
