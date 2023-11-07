022e8c31e9316073fb936ef88bb3dc96e5f5f207
thevorgx <kh.elallali@gmail.com>
2023-11-07 01:34:57 +0100

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""MyIint class"""
+
+
+class MyInt(int):
+    """MyInt"""
+    def __eq__(self, other):
+        """eq methode Override the == operator to invert its behavior"""
+        return (not super().__eq__(other))
+
+    def __ne__(self, other):
+        """ne methode Override the != operator to invert its behavior"""
+        return (not super().__ne__(other))
