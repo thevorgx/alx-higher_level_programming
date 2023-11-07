0f83bf36e41c27a7df0f0972bb6c43f036389ba5
thevorgx <kh.elallali@gmail.com>
2023-11-08 00:05:59 +0100

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    """pascal triangle building blocks"""
+    rlist = [[1]]
+    for i in range(n - 1):
+        tmp = [0] + rlist[-1] + [0]
+        row = []
+        for j in range(len(rlist[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        rlist.append(row)
+    return (rlist)
