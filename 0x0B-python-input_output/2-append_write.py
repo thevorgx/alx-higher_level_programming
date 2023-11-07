e2a25dfa4642436f1683308cbb83f2cb415131c6
thevorgx <kh.elallali@gmail.com>
2023-11-07 16:21:40 +0100

    update task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""append write"""
+
+
 def append_write(filename="", text=""):
+    """append write"""
     with open(filename, 'a', encoding='UTF-8') as f:
-        return(f.write(text))
+        return (f.write(text))
