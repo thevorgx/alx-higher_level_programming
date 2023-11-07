5f191c6eb28bf0fce72c0afc3baf661cde9bf678
thevorgx <kh.elallali@gmail.com>
2023-11-07 18:59:17 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""save to json"""
+import sys
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv[1:]
+file = "add_item.json"
+
+if os.path.exists(file):
+    plist = load_from_json_file(file)
+else:
+    plist = []
+
+plist += args
+
+save_to_json_file(plist, file)
