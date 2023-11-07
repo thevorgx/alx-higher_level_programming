e90bc5d84209501603837a85f73dcd67b2cc168d
thevorgx <kh.elallali@gmail.com>
2023-11-07 16:19:12 +0100

    update task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""write file"""
+
+
 def write_file(filename="", text=""):
+    """write file"""
     with open(filename, 'w', encoding='UTF-8') as f:
         return (f.write(text))
