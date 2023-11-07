dca07d21d7918d5ea992c4f2bcdb64e55b9823f5
thevorgx <kh.elallali@gmail.com>
2023-11-07 16:23:20 +0100

    update task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""to json string"""
+
+
 import json
+
+
 def to_json_string(my_obj):
+    """to json string"""
     j_string = json.dumps(my_obj)
-    return(j_string)
+    return (j_string)
