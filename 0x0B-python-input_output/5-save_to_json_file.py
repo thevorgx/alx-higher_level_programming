f3c2601de7273a36a3b7f0cbf2f78466587289ff
thevorgx <kh.elallali@gmail.com>
2023-11-07 06:08:06 +0100

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+import json
+def save_to_json_file(my_obj, filename):
+        j_string = json.dumps(my_obj)
+        with open(filename, 'w', encoding='UTF-8') as f:
+                return (f.write(j_string))
