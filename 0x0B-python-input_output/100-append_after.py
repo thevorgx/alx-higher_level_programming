69c3133a985dd0b07e33a43e70e1ca321a714670
thevorgx <kh.elallali@gmail.com>
2023-11-08 00:55:06 +0100

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append after"""
+    with open(filename, 'r', encoding='UTF-8') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w', encoding='UTF-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
