a2982abd074b6812dcb33c9ee12b8fac121f6906
thevorgx <kh.elallali@gmail.com>
2023-11-12 05:02:17 +0100

    task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,16 @@ class Rectangle(Base):
         """Return a string representation of the Rectangle"""
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} "
                 f"- {self.width}/{self.height}")
+
+    def update(self, *args):
+        """update rectangle class"""
+        if (len(args)) >= 1:
+            self.id = args[0]
+        if (len(args)) >= 2:
+            self.width = args[1]
+        if (len(args)) >= 3:
+            self.height = args[2]
+        if (len(args)) >= 4:
+            self.x = args[3]
+        if (len(args)) >= 5:
+            self.y = args[4]
