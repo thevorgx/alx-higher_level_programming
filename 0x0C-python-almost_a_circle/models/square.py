c9547f2a860a8fa6114a31386e61147a31b4eb3e
thevorgx <kh.elallali@gmail.com>
2023-11-12 06:01:25 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""import Rectangle class"""
+
+
+from models.rectangle import Rectangle
+
+
+"""square classe (inherite from Rectangle)"""
+
+
+class Square(Rectangle):
+    """square classe (inherite from Rectangle)"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of a Square"""
+        return (f"[Square] ({self.id}) "
+                f"{self.x}/{self.y} - {self.width}")
