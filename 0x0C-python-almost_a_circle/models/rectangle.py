cf381d81fb1ec3792c6aae1d6163c3e63c52bbc4
thevorgx <kh.elallali@gmail.com>
2023-11-12 05:47:42 +0100

    task 9 (thanks to Ercudu for the tip)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,15 +94,21 @@ class Rectangle(Base):
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} "
                 f"- {self.width}/{self.height}")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """update rectangle class"""
-        if (len(args)) >= 1:
-            self.id = args[0]
-        if (len(args)) >= 2:
-            self.width = args[1]
-        if (len(args)) >= 3:
-            self.height = args[2]
-        if (len(args)) >= 4:
-            self.x = args[3]
-        if (len(args)) >= 5:
-            self.y = args[4]
+        if args:
+            if (len(args)) >= 1:
+                self.id = args[0]
+            if (len(args)) >= 2:
+                self.width = args[1]
+            if (len(args)) >= 3:
+                self.height = args[2]
+            if (len(args)) >= 4:
+                self.x = args[3]
+            if (len(args)) >= 5:
+                self.y = args[4]
+        elif kwargs:
+            the_keys = ['id', 'width', 'height', 'x', 'y']
+            for key, value in kwargs.items():
+                if key in the_keys:
+                    setattr(self, key, value)
