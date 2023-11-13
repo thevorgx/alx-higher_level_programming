a7e27852b82ae6eafba66e8c814a5122edf205d5
thevorgx <kh.elallali@gmail.com>
2023-11-13 01:37:34 +0100

    task 11 and task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,32 @@ class Square(Rectangle):
         """Return a string representation of a Square"""
         return (f"[Square] ({self.id}) "
                 f"{self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """property size"""
+        return (self.height)
+
+    @size.setter
+    def size(self, value):
+        """property setter size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the class Square"""
+        if args:
+            if (len(args)) >= 1:
+                self.id = args[0]
+            if (len(args)) >= 2:
+                self.size = args[1]
+            if (len(args)) >= 3:
+                self.x = args[2]
+            if (len(args)) >= 4:
+                self.y = args[3]
+
+        elif kwargs:
+            the_keys = ['id', 'size', 'x', 'y']
+            for key, value in kwargs.items():
+                if key in the_keys:
+                    setattr(self, key, value)
