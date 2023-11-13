5fd792c88330549642e0cf42a844642d092e06d7
thevorgx <kh.elallali@gmail.com>
2023-11-13 02:41:13 +0100

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,10 +2,12 @@
 """Class Base"""
 
 
+import json
+
+
 class Base:
     """Class Base"""
     __nb_objects = 0
-
 
     def __init__(self, id=None):
         """class constructor __init__"""
@@ -14,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Dictionary to JSON string"""
+        if list_dictionaries is None:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
