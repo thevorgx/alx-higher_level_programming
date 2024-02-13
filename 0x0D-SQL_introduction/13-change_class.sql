ae0b29c177bdf741af4cc3889205fe48f407ce74
thevorgx <kh.elallali@gmail.com>
2024-02-13 23:45:35 +0100

    show average score

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- delete from 'second_table' everyone with a score less than 5
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- show score average as average
+SELECT AVG(score) AS average FROM second_table;
