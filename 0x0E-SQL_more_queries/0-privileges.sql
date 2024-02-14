5c545c9ce0651ab81273e63a2bafdacf35506c1a
thevorgx <kh.elallali@gmail.com>
2024-02-14 17:24:41 +0100

    update task 0 comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- show all privileges of MYSQL users : 'ser_0d_1' and 'ser_0d_1' on 'localhost' server
+-- show all privileges of MYSQL users : 'user_0d_1' and 'user_0d_1' on 'localhost' server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
