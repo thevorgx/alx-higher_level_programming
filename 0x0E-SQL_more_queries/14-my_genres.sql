fc34d4a73876861ed95f9be580547fe630772603
thevorgx <kh.elallali@gmail.com>
2024-02-15 02:41:10 +0100

    -- display shows with tv_genre.name = Comedy

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+ORDER BY tv_genres.name;
