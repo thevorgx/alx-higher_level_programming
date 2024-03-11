6fcefc182dd00ec0635bf52b5bad2a001bb674b3
thevorgx <kh.elallali@gmail.com>
2024-03-11 10:14:46 +0000

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+
+if (args.length === 3) {
+  for (let i = 0; i < args[2]; i++) {
+    console.log('C is fun');
+  }
+} else if (args.length === 2) {
+  console.log('Missing number of occurrences');
+}
