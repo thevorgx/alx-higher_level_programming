f6eba676a40485bad2e20c6ceab177d00a6dfacf
thevorgx <kh.elallali@gmail.com>
2024-03-11 23:30:56 +0000

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const args = process.argv;
+
+function factorial (num) {
+  num = parseInt(num);
+  if (num <= 1 || isNaN(num)) {
+    return (1);
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(args[2]));
