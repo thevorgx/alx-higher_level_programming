12a4986cab8ffd1bf48066b36241924592cba66f
thevorgx <kh.elallali@gmail.com>
2024-03-11 07:34:51 +0000

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv;
+
+if (args[2]) {
+  for (let i = 2; args[i] !== undefined; i++) {
+    console.log(args[i]);
+  }
+} else {
+  console.log('No argument');
+}
