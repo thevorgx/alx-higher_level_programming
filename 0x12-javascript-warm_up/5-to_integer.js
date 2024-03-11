00e3680e1f2c533cc46ae00e90c3129c98b9e7fc
thevorgx <kh.elallali@gmail.com>
2024-03-11 13:24:19 +0000

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 args[2] = parseInt(args[2]);
 
-if (!isNaN(args[2]) && args.length > 2) {
+if (!isNaN(args[2]) && args.length === 3) {
   console.log('My number: ' + args[2]);
 } else {
   console.log('Not a number');
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const args = process.argv;
+args[2] = parseInt(args[2]);
+
+if (!isNaN(args[2]) && args.length === 3) {
+  for (let i = 0; i < args[2]; i++) {
+    let line = '';
+    for (let j = 0; j < args[2]; j++) {
+      line += 'X ';
+    }
+    console.log(line);
+  }
+} else if (isNaN(args[2])) {
+  console.log('Missing size');
+}
