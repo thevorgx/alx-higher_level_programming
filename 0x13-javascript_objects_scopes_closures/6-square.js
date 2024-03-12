3e0974f547ab49ca89cb3fd98105e7cb8c5271be
thevorgx <kh.elallali@gmail.com>
2024-03-12 20:52:54 +0000

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -40,7 +40,7 @@ module.exports = class Square extends Rectangle {
       for (let i = 0; i < this.height; i++) {
         let line = '';
         for (let j = 0; j < this.width; j++) {
-          line += c;
+          line += 'C';
         }
         console.log(line);
       }
