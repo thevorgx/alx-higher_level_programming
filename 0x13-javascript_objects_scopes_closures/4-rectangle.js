8914e0cf4fc6b26a90a1ef37afd522810219fc8f
thevorgx <kh.elallali@gmail.com>
2024-03-12 17:57:23 +0000

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+    return (this.width, this.height);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+    return (this.width, this.height);
+  }
+};
