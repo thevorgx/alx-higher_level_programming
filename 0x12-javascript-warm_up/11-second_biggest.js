f90a18ad9f2d88ceff470eae12f3b8fffc7fbb89
thevorgx <kh.elallali@gmail.com>
2024-03-12 00:26:24 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+function secondBig (array) {
+  let biggest = array[0];
+  let secBiggest = -Infinity;
+  if (array.length <= 1) {
+    return (0);
+  }
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > biggest) {
+      secBiggest = biggest;
+      biggest = array[i];
+    } else if (array[i] < biggest && array[i] > secBiggest) {
+      secBiggest = array[i];
+    }
+  }
+  return secBiggest;
+}
+console.log(secondBig(args));
