df1923b04988384eb3f4b1f2e460c1c3f2625fec
thevorgx <kh.elallali@gmail.com>
2024-03-12 23:39:40 +0000

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; list[i] !== undefined; i++) {
+    if (searchElement === list[i]) {
+      count += 1;
+    }
+  }
+  return (count);
+};
