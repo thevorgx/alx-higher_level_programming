6aa5a066353e7363c8c26228c49ee1eed1fdcb04
thevorgx <kh.elallali@gmail.com>
2024-03-12 21:19:54 +0000

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-      for (let j = 0; j < this.width; j++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
-  }
-
-  rotate () {
-    const tmp = this.height;
-    this.height = this.width;
-    this.width = tmp;
-    return (this.width, this.height);
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-    return (this.width, this.height);
-  }
-}
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const SquareCode = require('./5-square');
+module.exports = class Square extends SquareCode {
   charPrint (c) {
     if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
