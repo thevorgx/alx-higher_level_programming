17fbf1aa3b1ac1410a2f1213bbdf36892ad0ef8b
thevorgx <kh.elallali@gmail.com>
2024-03-13 00:11:48 +0000

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const revList = [];
+  for (let i = list.length - 1; i !== -1; --i) {
+    revList.push(list[i]);
+  }
+  return (revList);
+};
