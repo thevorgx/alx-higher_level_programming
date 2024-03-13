d05d2fa2b758de1556d689f6867bb258ed3837ba
thevorgx <kh.elallali@gmail.com>
2024-03-13 02:16:44 +0000

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,6 +3,9 @@ const list = require('./100-data').list;
 let prev = [0];
 const newList = list.map(function (value) {
   const curr = value;
+  if (curr < 1) {
+    return (curr);
+  }
   const res = curr * prev;
   prev = curr;
   return (res);
