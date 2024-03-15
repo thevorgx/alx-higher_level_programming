e755346b805cc3c1cc32de802a93d265d4b11e23
thevorgx <kh.elallali@gmail.com>
2024-03-15 06:29:04 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""select states name that starts with 'N' by asc order"""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db_connexion = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                   db=argv[3], host='localhost', port=3306)
+    to_find = argv[4]
+    cursor = db_connexion.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    db_all_rows = cursor.fetchall()
+    for row in db_all_rows:
+        if row[1] == to_find:
+            print(row)
+    db_connexion.close()
