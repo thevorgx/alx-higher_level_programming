dd6bee4056dc1aa32e1e0bac4368f00b791e2aac
thevorgx <kh.elallali@gmail.com>
2024-03-15 06:56:49 +0000

    task 3, maybe i handled sql injection by accident

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""retrieves rows from the 'states' table based on a provided term to find"""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db_connexion = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                   db=argv[3], host='localhost', port=3306)
+    to_find = argv[4]
+    cursor = db_connexion.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id".format(to_find))
+    db_all_rows = cursor.fetchall()
+    for row in db_all_rows:
+        if row[1] == to_find:
+            print(row)
+    db_connexion.close()
