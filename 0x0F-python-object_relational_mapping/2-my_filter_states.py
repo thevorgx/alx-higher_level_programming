f0c0ac0e62bdfc88aa4a11174c343eb7cb35431e
thevorgx <kh.elallali@gmail.com>
2024-03-15 06:47:56 +0000

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                                    db=argv[3], host='localhost', port=3306)
     to_find = argv[4]
     cursor = db_connexion.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id;")
+    cursor.execute("SELECT * FROM states ORDER BY id".format(to_find))
     db_all_rows = cursor.fetchall()
     for row in db_all_rows:
         if row[1] == to_find:
