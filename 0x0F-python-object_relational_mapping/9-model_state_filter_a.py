f626bf570e54173e8e7deb2c4dcc926852d11df3
thevorgx <kh.elallali@gmail.com>
2024-03-16 22:38:16 +0000

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""retrieves states name with the id at the beginning,
+ordered by the state.id and only print it if it has 'a' using sqlalchemy"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                         argv[2], argv[3])
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
