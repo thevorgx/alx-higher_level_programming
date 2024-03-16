872f8852162be76e89e9d548f9c989cff09d53d7
thevorgx <kh.elallali@gmail.com>
2024-03-16 23:25:31 +0000

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""querie to retrieves all states print the state.id matching
+the state.name, print nothing ig not found using sqlalchemy"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                         argv[2], argv[3])
+    to_find = argv[4]
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+
+    states = session.query(State).all()
+    found = 0
+    for state in states:
+        if state.name == to_find:
+            print("{}".format(state.id))
+            found = 1
+    if found == 0:
+        print("Not found")
