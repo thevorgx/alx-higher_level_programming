4fb9863ed87f8e721c62a19ac0e7978f496b20f8
thevorgx <kh.elallali@gmail.com>
2024-03-16 00:37:51 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""retrieves rows from the 'states' table based on a provided term to find"""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db_connexion = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                                   db=argv[3], host='localhost', port=3306)
+    cursor = db_connexion.cursor()
+    cursor.execute("""SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;""")
+    db_all_rows = cursor.fetchall()
+    for row in db_all_rows:
+        print(row)
+    db_connexion.close()
