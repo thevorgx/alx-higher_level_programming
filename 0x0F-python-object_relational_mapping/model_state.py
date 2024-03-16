91ccf1a01bef7ec1f2b2260a18b10b759f54908d
thevorgx <kh.elallali@gmail.com>
2024-03-16 06:17:30 +0000

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from sqlalchemy import MetaData, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base(metadata=MetaData())
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
