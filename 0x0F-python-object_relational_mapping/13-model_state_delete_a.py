bfe0fdb873a2d33bdbb870af8a890df1a68ba27b
thevorgx <kh.elallali@gmail.com>
2024-03-17 01:25:26 +0000

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
 
     make_session = sessionmaker(bind=engine)
     session = make_session()
-    states = session.query(State).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
-        if 'a' in state.name:
-            session.delete(state)
-            session.commit()
+        session.delete(state)
+    session.commit()
