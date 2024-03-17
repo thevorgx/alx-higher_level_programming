75c396c223641c152cbd50be8512cb9aa1d67c06
thevorgx <kh.elallali@gmail.com>
2024-03-17 01:11:52 +0000

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""update the state name with the id '2' to New Mexico"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                         argv[2], argv[3])
+    engine = create_engine(db_url)
+
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
+    session.commit()
