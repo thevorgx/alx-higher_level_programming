7128bb9a8a55a8055ea45d7b80c37f62a094c909
thevorgx <kh.elallali@gmail.com>
2024-03-17 01:19:51 +0000

    task 14

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""delete a state if the name of the state have 'a' in it"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                         argv[2], argv[3])
+    engine = create_engine(db_url)
+
+    make_session = sessionmaker(bind=engine)
+    session = make_session()
+    states = session.query(State).all()
+    for state in states:
+        if 'a' in state.name:
+            session.delete(state)
+            session.commit()
