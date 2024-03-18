15196c8b4128043fb1641603c2331482b6210143
thevorgx <kh.elallali@gmail.com>
2024-03-18 02:53:43 +0000

    update

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                          argv[2], argv[3])
     engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
 
     make_session = sessionmaker(bind=engine)
     session = make_session()
