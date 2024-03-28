54dff7dc71818ddc1d1b3f3946e61b6b615279ed
thevorgx <kh.elallali@gmail.com>
2024-03-28 11:45:55 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = argv[1]
     try:
         with urlopen(url, timeout=10) as response:
-            res = response.read().decode("utf-8")
+            res = response.read().decode("ascii")
             print(response)
     except HTTPError as error:
-        print("Error code: {}".format(error.code))
+        print("Error code: {}".format(error.status))
