ab27045e41fe2641b139206c2be9c628078b8804
thevorgx <kh.elallali@gmail.com>
2024-03-28 12:19:13 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""What's my status"""
+
+
+import requests
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    content = req.text
+    print("Body response:")
+    print("{}- type: {}".format("\t", type(content)))
+    print("{}- content: {}".format("\t", content))
