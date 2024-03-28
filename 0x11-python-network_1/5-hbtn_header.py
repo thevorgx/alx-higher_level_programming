a9fd1182b4cce984b6d822f4e015f842d735fc00
thevorgx <kh.elallali@gmail.com>
2024-03-28 22:08:19 +0000

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Response header value using requests"""
+
+
+from sys import argv
+from requests import get
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = "X-Request-Id"
+    res = get(url, data=value)
+    print(res.headers.get(value))
