44ecce927a4a4db5804ccc8d5d0629a4813baa26
thevorgx <kh.elallali@gmail.com>
2024-03-28 23:29:08 +0000

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""catch the http error"""
+
+
+from sys import argv
+from requests.exceptions import HTTPError
+from requests import get
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        res = get(url)
+        res.raise_for_status()
+        print(res.text)
+    except HTTPError as error:
+        print("Error code: {}".format(error.response.status_code))
