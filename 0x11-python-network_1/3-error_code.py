7a7256dc14e3cf4db6260e6da0e2d4ca20af9d40
thevorgx <kh.elallali@gmail.com>
2024-03-28 11:48:17 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen
 if __name__ == "__main__":
     url = argv[1]
     try:
-        with urlopen(url, timeout=10) as response:
+        with urlopen(url) as response:
             res = response.read().decode("ascii")
             print(response)
     except HTTPError as error:
