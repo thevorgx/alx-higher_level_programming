e58a0f10fcc979f29b972fd5c8ea9455055bc83c
thevorgx <kh.elallali@gmail.com>
2024-03-28 02:43:17 +0000

    task 6, still issues with output

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""find peeak number in a list"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    for i in range(len(list_of_integers)):
+        current_element = list_of_integers[i]
+
+        if i == 0:
+            continue
+
+        left_value = list_of_integers[i - 1]
+        right_value = list_of_integers[i + 1]
+
+        if current_element >= left_value and current_element >= right_value:
+            return current_element
