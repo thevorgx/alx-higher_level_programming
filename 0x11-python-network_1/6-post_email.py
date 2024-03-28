bd402460fb927e229d1cdb6658f4c3545d41b963
thevorgx <kh.elallali@gmail.com>
2024-03-28 22:38:04 +0000

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""post an email, and get it back"""
+
+
+from sys import argv
+from requests import post
+
+if __name__ == "__main__":
+    url = argv[1]
+    email_value = argv[2]
+    data = {'email': email_value}
+    req = post(url, data=data)
+    print(req.content)
