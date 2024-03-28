54f559d30eb4ab07b08d3fd884a07002130d1bd0
thevorgx <kh.elallali@gmail.com>
2024-03-28 00:45:28 +0000

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display the body of the response only if status code is 200
-[ "$(curl -o /dev/null -s -w "%{http_code}" "$1")" -eq 403 ] && curl -sL "$1"
+[ "$(curl -o /dev/null -s -w "%{http_code}" "$1")" -eq 200 ] && curl -sL "$1"
