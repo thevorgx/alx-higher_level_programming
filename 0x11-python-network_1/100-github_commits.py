a0774ff451de23ea31a742d1c3168f448c130307
thevorgx <kh.elallali@gmail.com>
2024-03-29 03:07:14 +0000

    advanced 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""comment to update later"""
+
+
+from sys import argv
+from requests import get
+
+if __name__ == "__main__":
+    user = argv[1]
+    repo = argv[2]
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
+    res = get(url)
+    data = res.json()
+    count = 1
+
+    for commit in data:
+        if count <= 30:
+            commit_data = commit.get('commit')
+            commiter = commit_data.get('committer').get('name')
+            sha = commit_data.get('tree').get('sha')
+            print("{}: {}".format(sha, commiter))
+            count += 1
+        else:
+            break
