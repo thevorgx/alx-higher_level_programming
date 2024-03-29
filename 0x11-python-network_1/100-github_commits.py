25ddd667a85bf60397b2e8d52ebc6259f714ea49
thevorgx <kh.elallali@gmail.com>
2024-03-29 05:56:54 +0000

    idk anymore

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
         for commit in data:
             commit_data = commit.get('commit')
             commiter = commit_data.get('author').get('name')
-            sha = commit_data.get('tree').get('sha')
+            sha = commit.get('sha')
             print("{}: {}".format(sha, commiter))
