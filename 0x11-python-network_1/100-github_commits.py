e94a69ae5403b38eb99b5e4acd87ff631c5a85f0
thevorgx <kh.elallali@gmail.com>
2024-03-29 04:53:52 +0000

    per_page param instead of count: hint -> https://github.com/orgs/community/discussions/24371

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,17 +8,11 @@ from requests import get
 if __name__ == "__main__":
     user = argv[1]
     repo = argv[2]
-    url = f"https://api.github.com/repos/{user}/{repo}/commits"
+    url = f"https://api.github.com/repos/{user}/{repo}/commits?per_page=10"
     res = get(url)
     data = res.json()
-    count = 1
-    data.reverse()
     for commit in data:
-        if count <= 10:
-            commit_data = commit.get('commit')
-            commiter = commit_data.get('author').get('name')
-            sha = commit.get('sha')
-            print("{}: {}".format(sha, commiter))
-            count += 1
-        else:
-            break
+        commit_data = commit.get('commit')
+        commiter = commit_data.get('author').get('name')
+        sha = commit.get('sha')
+        print("{}: {}".format(sha, commiter))
